8fb41c238be3fa833f56c7731746cbf35a8e23c0
Vzaa <Vzaa@users.noreply.github.com>
2018-12-09 23:02:04 +0300

    Use slices instead of begin

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,33 +1,33 @@
 use std::fs;
 
-fn part1(vals: &[usize], begin: usize) -> (usize, usize) {
-    let (children, meta_cnt) = (vals[begin + 0], vals[begin + 1]);
+fn part1(vals: &[usize]) -> (usize, usize) {
+    let (children, meta_cnt) = (vals[0], vals[1]);
     let mut tot_len = 2;
     let mut child_sum = 0;
 
     for _ in 0..children {
-        let (l, s) = part1(vals, begin + tot_len);
+        let (l, s) = part1(&vals[tot_len..]);
         tot_len += l;
         child_sum += s;
     }
 
-    let own_sum: usize = vals[begin + tot_len..].iter().take(meta_cnt).sum();
+    let own_sum: usize = vals[tot_len..].iter().take(meta_cnt).sum();
 
     (tot_len + meta_cnt, child_sum + own_sum)
 }
 
-fn part2(vals: &[usize], begin: usize) -> (usize, usize) {
-    let (children, meta_cnt) = (vals[begin + 0], vals[begin + 1]);
+fn part2(vals: &[usize]) -> (usize, usize) {
+    let (children, meta_cnt) = (vals[0], vals[1]);
     let mut tot_len = 2;
 
     let mut child_ids = vec![];
     for _ in 0..children {
-        let (l, id) = part2(vals, begin + tot_len);
+        let (l, id) = part2(&vals[tot_len..]);
         tot_len += l;
         child_ids.push(id);
     }
 
-    let metadata = vals[begin + tot_len..].iter().take(meta_cnt);
+    let metadata = vals[tot_len..].iter().take(meta_cnt);
 
     if children == 0 {
         let own_sum: usize = metadata.sum();
@@ -42,9 +42,9 @@ fn main() {
     let dat = fs::read_to_string("input").unwrap();
     let vals: Vec<usize> = dat.split_whitespace().map(|x| x.parse().unwrap()).collect();
 
-    let fin = part1(&vals, 0);
+    let fin = part1(&vals);
     println!("Part 1: {}", fin.1);
 
-    let fin = part2(&vals, 0);
+    let fin = part2(&vals);
     println!("Part 2: {}", fin.1);
 }
